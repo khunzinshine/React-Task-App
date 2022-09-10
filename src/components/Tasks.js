21dae74b1120b581e8cf19ed068de941a3cefbdc
khunzinshine <khunzinshine@ayainnovation.com>
2022-09-10 12:34:59 +0630

    modify color and views

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import Task from './Task';
-import "../index.css"
+import React from "react";
+import Task from "./Task";
+import "../index.css";
 const Tasks = ({ tasks, onDelete, onEdit }) => {
   return (
     <>
-      {
-        tasks.map((task) => (
-          <Task key={task.id} task={task} onDelete={onDelete} onEdit={onEdit} />
-        ))
-      }
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onEdit={onEdit} />
+      ))}
     </>
-    )
-}
+  );
+};
 export default Tasks;
